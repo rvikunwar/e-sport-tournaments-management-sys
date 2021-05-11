5330dcd54761f876e42ed43164df98f65e2bbc5a
unknown <ravikunwar079@gmail.com>
2021-05-11 20:23:20 +0530

    mm

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
-const base_url = "http://127.0.0.1:8000/api/"
+const base_url = "https://backkendd.herokuapp.com/api/"
 
 const axiosInstance = axios.create({
     baseURL: base_url,
